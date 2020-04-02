fabbfe59794b43989f79ac8f55f2be5a5b12fa39
dlshoong <dlshoong@users.noreply.github.com>
2020-04-02 17:23:00 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,26 +4,46 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-
-  request('https://jsonplaceholder.typicode.com/users', function(error, response, body) {
-    console.log(response.json(body));
-  });
-})
-
-app.get('/users', function(req, res) {
-  request('https://jsonplaceholder.typicode.com/users', function(error, response, body) {
-    response.send(response.json(body));
-  });
-});
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
+const request = require ('request');
+const querystring = require ('querystring');
 
+let dataString = 'grant_type = authorization_code'
++ '& client_id = b4bae52f31781e2c403c049bc2007903'
++ '& redirect_uri = http://google.com/oauth/kakao'
++ '& code = SeKvFRI8a_Uiau0Ue1A-Cha4KJS251tGf3QHDJ5OkfV0gmSVDDdq45Xbth83W7G_QBKMjAorDNIAAAFxOi-3qw'
+
+let options = {
+url: ' https://kauth.kakao.com/oauth/token',
+method: 'POST',
+headers: {
+Content_Type: 'application / x-www-form-urlencoded'
+},
+body: dataString
+};
+
+function callback (error, response, body) {
+console.log (response.statusCode);
+
+if(!error && response.statusCode==200) {
+	console.log(body);
+} else {
+	console.log('error : ' + error);
+	console.log('body : ' + body);
+}
+}
+
+
+
+
+/*
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+*/
 /*
 const https = require('https');
 
