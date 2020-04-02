c7bb934a8db9e146a3854d0a012392b3e052984b
dlshoong <dlshoong@users.noreply.github.com>
2020-04-02 16:29:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+/*
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
@@ -11,3 +12,24 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+*/
+
+
+const https = require('https');
+
+https.get('https://kauth.kakao.com/oauth/authorize?client_id=b4bae52f31781e2c403c049bc2007903&redirect_uri=http://google.com/oauth/kakao&response_type=code&scope=friends,talk_message,profile', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
