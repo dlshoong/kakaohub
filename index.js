c514eacc058317759483f6865369e440564cb943
dlshoong <dlshoong@users.noreply.github.com>
2020-04-02 18:25:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); //returns 'february'
+
+/*
 var express = require('express')
 var app = express()
 
@@ -8,42 +21,13 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-const request = require ('request');
-const querystring = require ('querystring');
-
-let dataString = 'grant_type = authorization_code'
-+ '& client_id = b4bae52f31781e2c403c049bc2007903'
-+ '& redirect_uri = http://google.com/oauth/kakao'
-+ '& code = SeKvFRI8a_Uiau0Ue1A-Cha4KJS251tGf3QHDJ5OkfV0gmSVDDdq45Xbth83W7G_QBKMjAorDNIAAAFxOi-3qw'
-
-let options = {
-url: ' https://kauth.kakao.com/oauth/token',
-method: 'POST',
-headers: {
-Content_Type: 'application / x-www-form-urlencoded'
-},
-body: dataString
-};
-
-function callback (error, response, body) {
-console.log (response.statusCode);
-
-if(!error && response.statusCode==200) {
-	console.log(body);
-} else {
-	console.log('error : ' + error);
-	console.log('body : ' + body);
-}
-}
-
-
-
-
-/*
 app.get('/', function(request, response) {
   response.send('Hello World!')
+  
 })
 */
+
+
 /*
 const https = require('https');
 
