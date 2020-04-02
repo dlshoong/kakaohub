2f6ed12a437cb3c2d601b344c5ffd96cf180f92e
dlshoong <dlshoong@users.noreply.github.com>
2020-04-02 16:41:52 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-/*
+
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/users', function(req, res) {
+  request('https://jsonplaceholder.typicode.com/users', function(error, response, body) {
+      res.json(body)
+  });
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-*/
 
 
+/*
 const https = require('https');
 
 https.get('https://kauth.kakao.com/oauth/authorize?client_id=b4bae52f31781e2c403c049bc2007903&redirect_uri=http://google.com/oauth/kakao&response_type=code&scope=friends,talk_message,profile', (resp) => {
@@ -33,3 +39,4 @@ https.get('https://kauth.kakao.com/oauth/authorize?client_id=b4bae52f31781e2c403
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
+*/
