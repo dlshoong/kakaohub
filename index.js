acb6f0d6d8075251536b23711bad3a81ebd7d14c
dlshoong <dlshoong@users.noreply.github.com>
2020-04-09 16:51:02 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ apiRouter.post('/showHello', function(req, res) {
   res.status(200).send(responseBody);
 });
 
-app.listen(3000, function() {
+app.listen(80, function() {
   console.log('Example skill server listening on port 3000!');
 });
